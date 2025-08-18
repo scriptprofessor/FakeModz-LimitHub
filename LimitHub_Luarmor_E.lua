a50f128b88f97d0e21d4911a1336140395dc3c64
ScriptProfessor <scriptprofessor085@gmail.com>
2025-08-18 17:44:00 +0800

    Update LimitHub_Luarmor_E.lua

--- LimitHub_Luarmor_E.lua
+++ LimitHub_Luarmor_E.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet("https://pastefy.app/0fFQ3SzA/raw"))()
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/FakeModz/LimitHub/refs/heads/main/LimitHub_Luarmor_E.lua')))()
+
